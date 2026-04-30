a06f0aea1c3476625ba5d17961e44b96e4bd5c2c
kunlii <likun@mail.bnu.edu.cn>
2026-04-30 09:56:02 +0000

    Deploying to gh-pages from @ kunlii/kunlii.github.io@9aaccb9311fa6fc277d1e7a725646685bf42309e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-dataset-ownership-in-the-era-of-large-language-models-was-accepted-by-bwtac-2025",
+          title: 'Dataset Ownership in the Era of Large Language Models was accepted by BWTAC...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
