3f9d9fa9b8860e70ba9d8bde3af0f3d6852f3d48
kunlii <likun@mail.bnu.edu.cn>
2026-05-09 07:24:07 +0000

    Deploying to gh-pages from @ kunlii/kunlii.github.io@dde213f3e67762b6fd2975f04b861108c7e83688 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -413,11 +413,6 @@ ninja.data = [{
           description: "山东省重点研发计划（竞争性创新平台）-课题负责人",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
